2257e16619f87ab780ff5d2d8a1c3b61b671e914
Muhammad Awais Yaseen <85793305+muhammadawaisyaseen@users.noreply.github.com>
2022-12-06 20:25:06 +0500

    stack update

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +9,41 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('awais')),
-    );
+        appBar: AppBar(
+          title: const Text("Flutter"),
+        ),
+        body: Stack(
+          // alignment: Alignment.bottomCenter,
+          children: [
+            // Red Container
+            Container(
+              width: 200,
+              height: 300,
+              color: Colors.red,
+            ),
+
+            // Green Container
+            Positioned(
+              left: 10,
+              bottom: 20,
+              child: Container(
+                width: 40,
+                height: 100,
+                color: Colors.green,
+              ),
+            ),
+
+            // Black Container
+            Positioned(
+              left: 10,
+              bottom: 20,
+              child: Container(
+                width: 40,
+                height: 100,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ));
   }
 }
