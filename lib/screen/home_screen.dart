3e98321fb860e8e7c11c4875c6e3ee1f2c05065e
Muhammad Awais Yaseen <85793305+muhammadawaisyaseen@users.noreply.github.com>
2022-12-06 21:17:40 +0500

    stack_coloring

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,38 +11,86 @@ class HomeScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Flutter"),
         ),
-        body: Stack(
-          // alignment: Alignment.bottomCenter,
-          children: [
-            // Red Container
-            Container(
-              width: 200,
-              height: 300,
-              color: Colors.red,
-            ),
-
-            // Green Container
-            Positioned(
-              left: 10,
-              bottom: 20,
-              child: Container(
-                width: 40,
-                height: 100,
-                color: Colors.green,
-              ),
-            ),
-
-            // Black Container
-            Positioned(
-              left: 10,
-              bottom: 20,
-              child: Container(
-                width: 40,
-                height: 100,
+        body: Padding(
+          padding: EdgeInsets.all(30),
+          child: Stack(
+            
+            children: [
+              // Black Container
+              Container(
+                width: 500,
+                height: 600,
                 color: Colors.black,
               ),
-            ),
-          ],
-        ));
+
+              // Red Container
+              Positioned(
+                left: 70,
+                top: 180,
+                child: Container(
+                  width: 80,
+                  height: 250,
+                  color: Colors.red,
+                ),
+              ),
+
+              // Yellow Container
+              Positioned(
+                top: 260,
+                left: 20,
+                child: Container(
+                  width: 400,
+                  height: 80,
+                  color: Colors.yellow,
+                ),
+              ),
+
+              // Green Container
+              Positioned(
+                right: 70,
+                top: 180,
+                child: Container(
+                  width: 80,
+                  height: 250,
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          ),
+        )
+        //  Stack(
+        //   // alignment: Alignment.bottomCenter,
+        //   children: [
+        //     // Red Container
+        //     Container(
+        //       width: 200,
+        //       height: 300,
+        //       color: Colors.red,
+        //     ),
+
+        //     // Green Container
+        //     Positioned(
+        //       left: 10,
+        //       bottom: 20,
+        //       child: Container(
+        //         width: 40,
+        //         height: 100,
+        //         color: Colors.green,
+        //       ),
+        //     ),
+
+        //     // Black Container
+        //     Positioned(
+        //       left: 10,
+        //       bottom: 20,
+        //       child: Container(
+        //         width: 40,
+        //         height: 100,
+        //         color: Colors.black,
+        //       ),
+        //     ),
+        //   ],
+        // )
+        );
   }
 }
