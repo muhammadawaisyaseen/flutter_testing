fd1476cd1ee11a2b26012d44d1d61864c37dd711
Muhammad Awais Yaseen <85793305+muhammadawaisyaseen@users.noreply.github.com>
2022-12-08 19:10:13 +0500

    appbar_theme

--- lib/screen/lisview_testing.dart
+++ lib/screen/lisview_testing.dart
@@ -9,26 +9,58 @@ class ListviewTesing extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // return Padding(
+    //   padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+    //   child: ListView.builder(
+    //     // scrollDirection: Axis.vertical,
+    //     itemCount: 40,
+    //     itemBuilder: (BuildContext context, index) {
+    //       return Padding(
+    //         padding: const EdgeInsets.all(8.0),
+    //         child: Container(
+    //           // width: 200,
+    //           // height: MediaQuery.of(context).size.height * 0.08,
+    //           height: 90,
+    //           decoration: const BoxDecoration(
+    //               color: Colors.red,
+    //               borderRadius: BorderRadius.all(Radius.circular(30))),
+    //           child: ListTile(
+    //             leading: const Text("SAMSUNG"),
+    //             trailing: const Text("samsung"),
+    //             subtitle: const Text("GalaxyA51"),
+    //             // focusColor: Colors.red,
+    //             tileColor: Colors.green,
+    //             // selectedColor: Colors.orange,
+    //             //hoverColor: Colors.black,
+    //             onTap: () {
+    //               // Navigator.push(context,
+    //               //     MaterialPageRoute(builder: (context) => StackTesting()));
+    //             },
+    //           ),
+    //         ),
+    //       );
+    //     },
+    //   ),
+    // );
+    // return Text(
+    //   'Working',
+    //   style: TextStyle(
+    //     color: Colors.black,
+    //     fontWeight: FontWeight.w400,
+    //     fontSize: 14,
+    //     decoration: TextDecoration.none,
+    //   ),
+    // );
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("ListView"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
-        child: ListView.builder(
-          itemCount: 60,
-          itemBuilder: (BuildContext context, index) {
-            return ListTile(
-              leading: Text("SAMSUNG"),
-              trailing: Text("samsung"),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => StackTesting()));
-              },
-            );
-          },
+      appBar: AppBar(),
+      body: Text(
+        'Working',
+        style: TextStyle(
+          fontSize: 14,
+          color: Theme.of(context).appBarTheme.backgroundColor,
         ),
       ),
     );
+    // return Scaffold(body: Icon(Icons.add));
   }
 }
